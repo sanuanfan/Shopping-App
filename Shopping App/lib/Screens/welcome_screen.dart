5dccf7eb8f8cce9bb5c97d82d1c1933c6ee8afb6
sanuanfan <sanuanfan8535@gmail.com>
2024-07-11 00:40:03 +0530

    welcome screen

--- Shopping App/lib/Screens/welcome_screen.dart
+++ Shopping App/lib/Screens/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shopping_app/Screens/home_screen.dart';
 
 class welcome_screen extends StatelessWidget {
   const welcome_screen({super.key});
@@ -9,14 +11,61 @@ class welcome_screen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Container(
+        padding: EdgeInsets.only(top: 100, bottom: 40),
         decoration: BoxDecoration(
           color: Colors.black,
           image: DecorationImage(
               image: AssetImage("assets/images/bg.png"),
               fit: BoxFit.cover,
-              opacity: 0.5),
+              opacity: 0.6),
         ),
-        
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Coffee Shop",
+              style: GoogleFonts.pacifico(
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+            Column(
+              children: [
+                Text(
+                  "Need energy..? Take a sip..",
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 20, wordSpacing: 1),
+                ),
+                SizedBox(
+                  height: 80,
+                ),
+                InkWell(
+                  splashColor: Colors.black,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home_screen()),
+                    );
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(188, 239, 110, 4),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
